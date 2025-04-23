03a61c575fe72550f4aaf4391a43b8ab0e9902dd
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-23 08:59:31 -0400

    Intial commit - working cube jump

---
+++ Assets/Jump.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
